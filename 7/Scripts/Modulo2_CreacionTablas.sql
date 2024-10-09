131d87704dc0ffac6e2badbd5ab0aaf97efc6496
XaviHB07 <xavihuamanbonifacio@gmail.com>
2024-10-09 10:52:54 -0500

    new modulo 2 queries

---
+++ 7/Scripts/Modulo2_CreacionTablas.sql
@@ -0,0 +1,256 @@
+DROP TABLE IF EXISTS CATEGORIA CASCADE;
+CREATE TABLE CATEGORIA (
+  cod_categoria CHAR (5) NOT NULL,
+  nombre_categoria VARCHAR(30) NOT NULL,
+  PRIMARY KEY (cod_categoria)
+);
+
+DROP TABLE IF EXISTS Area CASCADE;
+CREATE TABLE Area (
+  Cod_area CHAR (10) NOT NULL,
+  nombre_area VARCHAR(100) NOT NULL,
+  PRIMARY KEY (cod_area)
+);
+
+DROP TABLE IF EXISTS Cargo CASCADE;
+CREATE TABLE Cargo (
+  Cod_cargo CHAR (10) NOT NULL,
+  Nombre_cargo VARCHAR(100) NOT NULL,
+  PRIMARY KEY (Cod_cargo)
+);
+
+DROP TABLE IF EXISTS Tipo_Contrato CASCADE;
+CREATE TABLE Tipo_Contrato (
+  Cod_tipo_contrato CHAR (10) NOT NULL,
+  Nombre_tipo_contrato VARCHAR(100) NOT NULL,
+  PRIMARY KEY (Cod_tipo_contrato)
+);
+
+DROP TABLE IF EXISTS Local CASCADE;
+CREATE TABLE Local (
+  Cod_local CHAR (10) NOT NULL,
+  Ubicacion_local VARCHAR(100) NOT NULL,
+  Nombre_local VARCHAR(100) NOT NULL,
+  PRIMARY KEY (Cod_local)
+);
+
+DROP TABLE IF EXISTS Turno CASCADE;
+CREATE TABLE Turno (
+  Cod_turno CHAR (10) NOT NULL,
+  Nombre_turno VARCHAR(100) NOT NULL,
+  Hora_inicio TIME NOT NULL,
+  Hora_fin TIME NOT NULL,
+  PRIMARY KEY (Cod_turno)
+);
+
+DROP TABLE IF EXISTS Producto_Friday CASCADE;
+CREATE TABLE Producto_Friday (
+  Cod_prodFriday CHAR (10) NOT NULL,
+  Nombre_producto VARCHAR(50) NOT NULL,
+  Precio_producto NUMERIC(10,2) NOT NULL,
+  Descripcion VARCHAR(100) NOT NULL,
+  Valoracion NUMERIC(10,2) NOT NULL,
+  cod_categoria CHAR (5) NOT NULL,
+  PRIMARY KEY (Cod_prodFriday)
+);
+
+DROP TABLE IF EXISTS Contrato CASCADE;
+CREATE TABLE Contrato (
+  Cod_contrato CHAR (10) NOT NULL,
+  Fecha_contratacion DATE NOT NULL,
+  Cod_tipo_contrato CHAR (10) NOT NULL,
+  PRIMARY KEY (Cod_contrato)
+);
+
+DROP TABLE IF EXISTS Empleado CASCADE;
+CREATE TABLE Empleado (
+  Direccion VARCHAR(50) NOT NULL,
+  Contacto CHAR (9) NOT NULL,
+  DNI CHAR (8) NOT NULL,
+  Primer_nombre VARCHAR(50) NOT NULL,
+  Primer_apellido VARCHAR(50) NOT NULL,
+  Segundo_apellido VARCHAR(50) NOT NULL,
+  Codigo_empleado CHAR (10) NOT NULL,
+  Cod_turno CHAR (10) NOT NULL,
+  Cod_contrato CHAR (10) NOT NULL,
+  Cod_area CHAR (10) NOT NULL,
+  Cod_cargo CHAR (10) NOT NULL,
+  Cod_local CHAR (10) NOT NULL,
+  PRIMARY KEY (Codigo_empleado)
+);
+
+DROP TABLE IF EXISTS TAMANO CASCADE;
+CREATE TABLE TAMANO (
+  cod_tamano CHAR (2) NOT NULL,
+  descripcion_tamano VARCHAR(20) NOT NULL,
+  PRIMARY KEY (cod_tamano)
+);
+
+DROP TABLE IF EXISTS TIPO_DE_MESA CASCADE;
+CREATE TABLE TIPO_DE_MESA (
+  cod_tipo_de_mesa CHAR (10) NOT NULL,
+  descripcion_tipo_mesa VARCHAR(20) NOT NULL,
+  PRIMARY KEY (cod_tipo_de_mesa)
+);
+
+DROP TABLE IF EXISTS ESTADO_PM CASCADE;
+CREATE TABLE ESTADO_PM (
+  cod_estado_pm CHAR (2) NOT NULL,
+  descripcion_estado_pm VARCHAR(20) NOT NULL,
+  PRIMARY KEY (cod_estado_pm)
+);
+
+DROP TABLE IF EXISTS ESTADO_DP CASCADE;
+CREATE TABLE ESTADO_DP (
+  cod_estado_dp CHAR (2) NOT NULL,
+  descripcion_estado_dp VARCHAR(20) NOT NULL,
+  PRIMARY KEY (cod_estado_dp)
+);
+
+DROP TABLE IF EXISTS AREA_EC CASCADE;
+CREATE TABLE AREA_EC (
+  cod_area_ec CHAR (2) NOT NULL,
+  descripcion_ec VARCHAR(30) NOT NULL,
+  PRIMARY KEY (cod_area_ec)
+);
+
+DROP TABLE IF EXISTS ESTADO_KDS CASCADE;
+CREATE TABLE ESTADO_KDS (
+  cod_estado_kds CHAR (2) NOT NULL,
+  descripcion_kds VARCHAR(20) NOT NULL,
+  PRIMARY KEY (cod_estado_kds)
+);
+
+DROP TABLE IF EXISTS ESTADO_ITEM_DP CASCADE;
+CREATE TABLE ESTADO_ITEM_DP (
+  cod_estado_item_dp CHAR (10) NOT NULL,
+  descripcion_estado_item_dp VARCHAR(20) NOT NULL,
+  PRIMARY KEY (cod_estado_item_dp)
+);
+
+DROP TABLE IF EXISTS ESTADO_IM CASCADE;
+CREATE TABLE ESTADO_IM (
+  cod_estado_im CHAR (10) NOT NULL,
+  descripcion_estado_im VARCHAR(20) NOT NULL,
+  PRIMARY KEY (cod_estado_im)
+);
+
+DROP TABLE IF EXISTS IDENTIFICACION_MESERO CASCADE;
+CREATE TABLE IDENTIFICACION_MESERO (
+  cod_im CHAR (10) NOT NULL,
+  fecha DATE NOT NULL,
+  hora TIME NOT NULL,
+  Codigo_empleado CHAR (10) NOT NULL,
+  cod_estado_im CHAR (10) NOT NULL,
+  PRIMARY KEY (cod_im)
+);
+
+DROP TABLE IF EXISTS MESA CASCADE;
+CREATE TABLE MESA (
+  cod_mesa CHAR (10) NOT NULL,
+  num_sillas NUMERIC(10,2) NOT NULL,
+  cod_tipo_de_mesa CHAR (10) NOT NULL,
+  cod_tamano CHAR (2) NOT NULL,
+  PRIMARY KEY (cod_mesa)
+);
+
+DROP TABLE IF EXISTS PEDIDO_DE_MESA CASCADE;
+CREATE TABLE PEDIDO_DE_MESA (
+  cod_pm CHAR (10) NOT NULL,
+  Fecha DATE NOT NULL,
+  Hora TIME NOT NULL,
+  cod_im CHAR (10) NOT NULL,
+  cod_mesa CHAR (10) NOT NULL,
+  cod_estado_pm CHAR (2) NOT NULL,
+  PRIMARY KEY (cod_pm)
+);
+
+DROP TABLE IF EXISTS DETALLE_PEDIDO CASCADE;
+CREATE TABLE DETALLE_PEDIDO (
+  cod_dp CHAR (10) NOT NULL,
+  precio_total NUMERIC(10,2) NOT NULL,
+  cantidad_total NUMERIC(10,2) NOT NULL,
+  Fecha DATE NOT NULL,
+  Hora TIME NOT NULL,
+  cod_pm CHAR (10) NOT NULL,
+  cod_estado_dp CHAR (2) NOT NULL,
+  PRIMARY KEY (cod_dp)
+);
+
+DROP TABLE IF EXISTS ITEM_DETALLE_PEDIDO CASCADE;
+CREATE TABLE ITEM_DETALLE_PEDIDO (
+  cod_item_dp CHAR (10) NOT NULL,
+  Cod_prodFriday CHAR (10) NOT NULL,
+  cantidad NUMERIC(10,2) NOT NULL,
+  precio NUMERIC(10,2) NOT NULL,
+  cod_dp CHAR (10) NOT NULL,
+  cod_estado_item_dp CHAR (10) NOT NULL,
+  PRIMARY KEY (cod_item_dp)
+);
+
+DROP TABLE IF EXISTS KDS CASCADE;
+CREATE TABLE KDS (
+  cod_kds CHAR (10) NOT NULL,
+  cod_area_ec CHAR (2) NOT NULL,
+  cod_estado_kds CHAR (2) NOT NULL,
+  cod_item_dp CHAR (10) NOT NULL,
+  PRIMARY KEY (cod_kds)
+);
+
+DROP TABLE IF EXISTS EMPLEADO_ITEMDETALLEPEDIDO CASCADE;
+CREATE TABLE EMPLEADO_ITEMDETALLEPEDIDO (
+  fecha DATE NOT NULL,
+  hora TIME NOT NULL,
+  Codigo_empleado CHAR (10) NOT NULL,
+  cod_item_dp CHAR (10) NOT NULL,
+  cod_estado_item_dp CHAR (2) NOT NULL,
+  PRIMARY KEY (Codigo_empleado, cod_item_dp)
+);
+
+ALTER TABLE Producto_Friday ADD FOREIGN KEY (cod_categoria) REFERENCES CATEGORIA (cod_categoria);
+
+ALTER TABLE Empleado ADD FOREIGN KEY (Cod_contrato) REFERENCES Contrato (Cod_contrato);
+
+ALTER TABLE Empleado ADD FOREIGN KEY (cod_area) REFERENCES Area (cod_area);
+
+ALTER TABLE Empleado ADD FOREIGN KEY (Cod_cargo) REFERENCES Cargo (Cod_cargo);
+
+ALTER TABLE Empleado ADD FOREIGN KEY (Cod_local) REFERENCES Local (Cod_local);
+
+ALTER TABLE Empleado ADD FOREIGN KEY (Cod_turno) REFERENCES Turno (Cod_turno);
+
+ALTER TABLE IDENTIFICACION_MESERO ADD FOREIGN KEY (Codigo_empleado) REFERENCES Empleado (Codigo_empleado);
+
+ALTER TABLE IDENTIFICACION_MESERO ADD FOREIGN KEY (cod_estado_im) REFERENCES ESTADO_IM (cod_estado_im);
+
+ALTER TABLE MESA ADD FOREIGN KEY (cod_tipo_de_mesa) REFERENCES TIPO_DE_MESA (cod_tipo_de_mesa);
+
+ALTER TABLE MESA ADD FOREIGN KEY (cod_tamano) REFERENCES TAMANO (cod_tamano);
+
+ALTER TABLE PEDIDO_DE_MESA ADD FOREIGN KEY (cod_im) REFERENCES IDENTIFICACION_MESERO (cod_im);
+
+ALTER TABLE PEDIDO_DE_MESA ADD FOREIGN KEY (cod_mesa) REFERENCES MESA (cod_mesa);
+
+ALTER TABLE PEDIDO_DE_MESA ADD FOREIGN KEY (cod_estado_pm) REFERENCES ESTADO_PM (cod_estado_pm);
+
+ALTER TABLE DETALLE_PEDIDO ADD FOREIGN KEY (cod_pm) REFERENCES PEDIDO_DE_MESA (cod_pm);
+
+ALTER TABLE DETALLE_PEDIDO ADD FOREIGN KEY (cod_estado_dp) REFERENCES ESTADO_DP (cod_estado_dp);
+
+ALTER TABLE ITEM_DETALLE_PEDIDO ADD FOREIGN KEY (cod_dp) REFERENCES DETALLE_PEDIDO (cod_dp);
+
+ALTER TABLE ITEM_DETALLE_PEDIDO ADD FOREIGN KEY (Cod_prodFriday) REFERENCES Producto_Friday (Cod_prodFriday);
+
+ALTER TABLE ITEM_DETALLE_PEDIDO ADD FOREIGN KEY (cod_estado_item_dp) REFERENCES ESTADO_ITEM_DP (cod_estado_item_dp);
+
+ALTER TABLE KDS ADD FOREIGN KEY (cod_area_ec) REFERENCES AREA_EC (cod_area_ec);
+
+ALTER TABLE KDS ADD FOREIGN KEY (cod_estado_kds) REFERENCES ESTADO_KDS (cod_estado_kds);
+
+ALTER TABLE KDS ADD FOREIGN KEY (cod_item_dp) REFERENCES ITEM_DETALLE_PEDIDO (cod_item_dp);
+
+ALTER TABLE EMPLEADO_ITEMDETALLEPEDIDO ADD FOREIGN KEY (Codigo_empleado) REFERENCES Empleado (Codigo_empleado);
+
+ALTER TABLE EMPLEADO_ITEMDETALLEPEDIDO ADD FOREIGN KEY (cod_item_dp) REFERENCES ITEM_DETALLE_PEDIDO (cod_item_dp);
+
+ALTER TABLE EMPLEADO_ITEMDETALLEPEDIDO ADD FOREIGN KEY (cod_estado_item_dp) REFERENCES ESTADO_ITEM_DP (cod_estado_item_dp);
---
+++ 7/Scripts/Modulo2_poblacion_inicial.sql
@@ -0,0 +1,101 @@
+INSERT INTO CATEGORIA (cod_categoria, nombre_categoria) VALUES 
+('AP', 'Appetizers'),
+('BS', 'Burgers & Sandwiches');
+
+INSERT INTO Turno (Cod_turno, Nombre_turno, Hora_inicio, Hora_fin)
+VALUES 
+('TAR', 'Tarde', '14:00:00', '22:00:00'),
+('MAN', 'Mañana', '08:00:00', '18:00:00');
+INSERT INTO Area (Cod_area, Nombre_area)
+VALUES 
+('ALM', 'Almacén'),
+('COC', 'Cocina');
+INSERT INTO Cargo (Cod_cargo, Nombre_cargo)
+VALUES 
+('AL', 'ALMACENERO'),
+('CO', 'COCINERO');
+INSERT INTO Tipo_contrato (Cod_tipo_contrato, Nombre_tipo_contrato)
+VALUES 
+('C001', 'Indefinido'),
+('C002', 'Temporal');
+INSERT INTO Local (Cod_local, Ubicacion_local, Nombre_local)
+VALUES 
+('AE', 'Av. Central 123', 'AEROPUERTO'),
+('LA', 'Av. Norte 456', 'LARCOMAR');
+
+INSERT INTO Contrato (Cod_contrato, Fecha_contratacion, Cod_tipo_contrato)
+VALUES 
+('CT001', '2023-09-01', 'C001'),
+('CT002', '2024-01-01', 'C002');
+INSERT INTO Empleado (Direccion, Contacto, DNI, Primer_nombre, Primer_apellido, Segundo_apellido, Codigo_empleado, Cod_turno, Cod_contrato, Cod_area, Cod_cargo, Cod_local)
+VALUES 
+('Av. Siempreviva 742', '987654321', '12345678', 'Juan', 'Pérez', 'López', 'PL12345678', 'MAN', 'CT001','ALM', 'AL', 'AE'),
+('Calle Falsa 123', '987654322', '87654321', 'María', 'González', 'Díaz', 'GD87654321', 'TAR', 'CT002', 'COC', 'CO', 'LA');
+
+INSERT INTO Producto_Friday (Cod_prodFriday, Nombre_producto, Precio_producto, Descripcion, Valoracion, cod_categoria)
+VALUES ('CHK5147856', 'Sesame Chicken Strips', 40.00, 'Tiritas de pollo empanizadas en pan japonés, en salsa glaze fridays', 3.20, 'AP');
+INSERT INTO Producto_Friday (Cod_prodFriday, Nombre_producto, Precio_producto, Descripcion, Valoracion, cod_categoria)
+VALUES ('BUR1254785', 'Bacon Cheeseburger', 38.00, 'Hamburguesa de 200gr, gratinada con queso Cheddar y cubierta con dos tiras de tocino',4.00,'BS');
+
+INSERT INTO TAMANO (cod_tamano, descripcion_tamano) VALUES 
+('PE', 'Pequeño'),
+('ME', 'Mediano');
+INSERT INTO TIPO_DE_MESA (cod_tipo_de_mesa, descripcion_tipo_mesa) VALUES 
+('BA', 'Bar'),
+('NR', 'No bar');
+
+INSERT INTO ESTADO_PM (cod_estado_pm, descripcion_estado_pm) VALUES 
+('DI', 'Disponible'),
+('ND', 'No disponible');
+
+INSERT INTO ESTADO_DP (cod_estado_dp, descripcion_estado_dp) VALUES 
+('LI', 'Listo'),
+('NL', 'No Listo');
+
+INSERT INTO AREA_EC (cod_area_ec, descripcion_ec) VALUES 
+('ES', 'Estación Sartén'),
+('EA', 'Estación Parrilla');
+
+INSERT INTO ESTADO_KDS (cod_estado_kds, descripcion_kds) VALUES 
+('AC', 'Activo'),
+('NT', 'No activo');
+
+INSERT INTO ESTADO_ITEM_DP (cod_estado_item_dp, descripcion_estado_item_dp) VALUES 
+('LI', 'Listo'),
+('NL', 'No Listo');
+
+INSERT INTO ESTADO_IM (cod_estado_im, descripcion_estado_im) VALUES 
+('SI', 'Autenticado'),
+('NO', 'No autenticado');
+
+INSERT INTO IDENTIFICACION_MESERO (cod_im, fecha, hora, Codigo_empleado, cod_estado_im) VALUES 
+('IM00000001', '2024-10-01', '12:00:00', 'GD87654321', 'SI'),
+('IM00000002', '2024-10-01', '12:30:00', 'GD87654321', 'SI');
+
+INSERT INTO MESA (cod_mesa, num_sillas, cod_tipo_de_mesa, cod_tamano) VALUES 
+('M0000001', 2, 'NR', 'PE'),
+('M0000002', 4, 'NR', 'ME');
+
+INSERT INTO PEDIDO_DE_MESA (cod_pm, Fecha, Hora, cod_im, cod_mesa, cod_estado_pm) VALUES 
+('PM000001', '2024-10-01', '12:01:20', 'IM00000001', 'M0000001', 'ND'),
+('PM000002', '2024-10-01', '12:31:06', 'IM00000002', 'M0000002', 'ND');
+
+INSERT INTO DETALLE_PEDIDO (cod_dp, precio_total, cantidad_total, Fecha, Hora, cod_pm, cod_estado_dp) VALUES 
+('DP000001', 40.00, 1, '2024-10-01', '12:01:45', 'PM000001', 'NL'),
+('DP000002', 76.00, 2, '2024-10-01', '12:32:01', 'PM000002', 'NL');
+
+INSERT INTO ITEM_DETALLE_PEDIDO (cod_item_dp, Cod_prodFriday, cantidad, precio, cod_dp, cod_estado_item_dp) VALUES 
+('IDP00001', 'CHK5147856', 1, 40.00, 'DP000001', 'NL'),
+('IDP00002', 'BUR1254785', 2, 76.00, 'DP000002', 'NL');
+
+INSERT INTO KDS (cod_kds, cod_area_ec, cod_estado_kds, cod_item_dp) VALUES 
+('KDS000ES', 'ES', 'AC', 'IDP00001'),
+('KDS000EA', 'EA', 'AC', 'IDP00002');
+
+INSERT INTO EMPLEADO_ITEMDETALLEPEDIDO (fecha, hora, Codigo_empleado, cod_item_dp, cod_estado_item_dp) VALUES 
+('2024-10-01', '12:07:00', 'GD87654321','IDP00001' , 'LI'),
+('2024-10-01', '12:37:00', 'GD87654321','IDP00002', 'LI');
+
+
+
+
