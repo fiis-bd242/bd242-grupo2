6194058930998a028507327700958d6293c7b019
Cristhian <cristhian.maylle.b@uni.pe>
2024-11-21 17:35:16 -0500

    Delete Proyecto directory

--- Proyecto/backend/app/__init__.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-from .routes import router
-
-
-def create_app():
-    app = Flask(__name__)
-    app.config.from_object("app.config.Config")
-    app.register_blueprint(router)
-    return app
--- Proyecto/backend/app/config.py
+++
@@ -1,2 +0,0 @@
-class Config:
-    DATABASE_URI = "postgresql://grupo2:usuario@localhost:5432/tgi_fridays"
