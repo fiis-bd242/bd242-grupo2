176b9d7e3352590c6b4fadb1a70dda87ca2001dc
Seteley <tejedasergio2004@gmail.com>
2024-11-25 01:01:46 -0500

    CORS

--- Proyecto/backend/app/__init__.py
+++ Proyecto/backend/app/__init__.py
@@ -7,13 +7,13 @@ def create_app():
     app.config.from_object("app.config.Config")
     app.register_blueprint(router)
 
-    # Permitir solicitudes de un solo origen
-    CORS(app, resources={r"/*": {"origins": "https://bd242-grupo2-1.onrender.com"}})
+    # Permitir solicitudes desde dos orígenes
+    CORS(app, resources={r"/*": {"origins": ["https://bd242-grupo2-1.onrender.com", "http://localhost:5000"]}})
 
     # Middleware para agregar encabezados CORS manualmente (por si acaso)
     @app.after_request
     def after_request(response):
-        response.headers['Access-Control-Allow-Origin'] = 'https://bd242-grupo2-1.onrender.com'
+        response.headers['Access-Control-Allow-Origin'] = 'https://bd242-grupo2-1.onrender.com, http://localhost:5000'
         response.headers['Access-Control-Allow-Methods'] = 'GET, POST, PUT, DELETE, OPTIONS'
         response.headers['Access-Control-Allow-Headers'] = 'Content-Type, Authorization'
         return response
