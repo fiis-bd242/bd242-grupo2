58c83819e5c19e1e31b756de407bac585407690d
XaviHB07 <xavihuamanbonifacio@gmail.com>
2024-11-27 12:26:18 -0500

    pc4_query

--- 11/11.2/Consultas para PC4/Consultas para PC4.sql
+++ 11/11.2/Consultas para PC4/Consultas para PC4.sql
@@ -1,6 +1,26 @@
 -- Modulo1
 
 -- Modulo 2
+SELECT idp.cod_item_dp, idp.cod_dp,pf.nombre_producto , 
+	idp.cantidad , idp.precio, dp.cod_im,dp.cod_mesa num_mesa,
+	e.primer_apellido || ' ' || SUBSTRING(e.segundo_apellido FROM 1 FOR 1) || '. ' || 
+	e.primer_nombre mesero, 
+	SUM(idp.precio) OVER (PARTITION BY idp.cod_dp) AS TOTAL
+	--  Suma de precios de todas las filas que contengan el mismo "cod_dp"
+FROM ITEM_DETALLE_PEDIDO idp
+LEFT JOIN Producto_Friday pf
+on idp.cod_prodfriday = pf.cod_prodfriday
+LEFT JOIN DETALLE_PEDIDO dp
+on dp.cod_dp = idp.cod_dp
+LEFT JOIN IDENTIFICACION_MESERO im 
+ON dp.cod_im = im.cod_im
+LEFT JOIN Empleado e 
+ON im.Codigo_empleado = e.Codigo_empleado
+WHERE idp.cod_dp = (
+		select cod_dp 
+		from DETALLE_PEDIDO
+		order by cod_dp desc
+		LIMIT 1)
 
 -- Modulo 3
 
